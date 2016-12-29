3881fd1178c06bb8f8b7263bef25917bdee8e717
yangpengfei_19881112 <yangpengfei@haitar.com>
2016-12-29 18:26:06 +0800

    comm

--- app.js
+++ app.js
@@ -14,22 +14,35 @@ app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'zhd' }))
+app.use(session({
+    secret: 'zhd',
+    cookie: {
+        maxAge: 1000 * 60 * 30
+    }
+}))
 
 
 var userinfo = require('./app/api/use.js');
 var matterInReg = require('./app/api/matter_in_reg.js');
 
+// 静态资料路径
+app.use(express.static(__dirname + '/app/public/'));
+
+//路由限制
 app.use(function(req, res, next) {
+    var url = req.originalUrl;
+    console.log(url);
+    if (url != "/login" && url != '/api/login' && !req.session.user) {
+        return res.redirect("/login");
+    }
     next();
-})
+});
 
 //api接口
 app.use('/api', userinfo);
 app.use('/api', matterInReg);
 
-// 静态资料路径
-app.use(express.static(__dirname + '/app/public/'));
+
 
 //页面路由 登录
 app.get('/login', function(req, res) {
@@ -38,7 +51,6 @@ app.get('/login', function(req, res) {
 
 //页面路由 matterInReg/add
 app.get('/matterInReg/add', function(req, res) {
-	console.log(req.session.user)
     res.sendfile(__dirname + '/app/public/views/add_matter_in_reg.html');
 })
 
--- app/api/use.js
+++ app/api/use.js
@@ -18,9 +18,11 @@ var common = require('../common/common.js');
 
 
 var sql = {
-    login: 'SELECT t_xt_yh.YH_ID, t_xt_yh.YH_DM, t_xt_yh.YH_MC, t_xt_yh.MM,t_xt_yh.XB, t_xt_yh.YH_LX, t_xt_yh.YH_JS, t_xt_yh.XH,t_xt_yh.YX_BJ, t_xt_yh.SC_BJ, t_xt_yh.LR_RQ, t_xt_yh.XG_RQ,t_xt_yh.CZRY_ID, t_xt_yh.EMAIL, t_xt_yh.BZ, t_xt_jg.JG_ID,t_xt_jg.JG_DM, t_xt_jg.JG_MC, t_xt_gw.GW_ID, t_xt_gw.GW_DM,t_xt_gw.GW_MC FROM t_xt_yh left join t_xt_jg_yh on t_xt_yh.yh_id=t_xt_jg_yh.yh_id left join t_xt_jg on t_xt_jg_yh.jg_id=t_xt_jg.jg_id left join t_xt_yh_gw on t_xt_yh.yh_id=t_xt_yh_gw.yh_id left join t_xt_gw on t_xt_gw.gw_id=t_xt_yh_gw.gw_dm WHERE t_xt_yh.YH_DM = ',
+    login: 'SELECT t_xt_yh.YH_ID, t_xt_yh.YH_DM, t_xt_yh.YH_MC, t_xt_yh.MM,t_xt_yh.XB, t_xt_yh.YH_LX, t_xt_yh.YH_JS, t_xt_yh.XH,t_xt_yh.YX_BJ, t_xt_yh.SC_BJ, t_xt_yh.LR_RQ, t_xt_yh.XG_RQ,t_xt_yh.CZRY_ID, t_xt_yh.EMAIL, t_xt_yh.BZ, t_xt_jg.JG_ID,t_xt_jg.JG_DM, t_xt_jg.JG_MC, t_xt_gw.GW_ID, t_xt_gw.GW_DM,t_xt_gw.GW_MC FROM t_xt_yh left join t_xt_jg_yh on t_xt_yh.yh_id=t_xt_jg_yh.yh_id left join t_xt_jg on t_xt_jg_yh.jg_id=t_xt_jg.jg_id left join t_xt_yh_gw on t_xt_yh.yh_id=t_xt_yh_gw.yh_id left join t_xt_gw on t_xt_gw.gw_id=t_xt_yh_gw.gw_dm WHERE t_xt_yh.YH_DM = ?',
+    role: 'SELECT t_xt_gncd.CD_DM,t_xt_gncd.CD_MC,t_xt_gncd.CD_LX,t_xt_gncd.SJCDDM,t_xt_gncd.GN_DM,t_xt_gncd.CD_XH,t_xt_gncd.YX_BJ,t_xt_gncd.CD_MS,t_xt_gncd.BZ,t_xt_gncd.GN_TB,t_xt_gncd.GN_TP,parent_t_xt_gncd.CD_MC ASPARENT_NAME,t_xt_gnzy.URL,t_xt_gnzy.GN_MC FROM t_xt_gncd t_xt_gncd LEFT JOIN t_xt_gncd parent_t_xt_gncd ON t_xt_gncd.SJCDDM = parent_t_xt_gncd.CD_DM LEFT JOIN t_xt_gnzy t_xt_gnzy ON t_xt_gnzy.GN_DM =t_xt_gncd.GN_DM inner join t_xt_yh_cd_js on t_xt_yh_cd_js.CD_DM=t_xt_gncd.CD_DM WHERE t_xt_yh_cd_js.YH_ID = ? order by t_xt_yh_cd_js.JS desc',
 }
 
+// 登录
 router.post('/login', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
@@ -32,11 +34,11 @@ router.post('/login', function(req, res) {
         res.json(common.resErrorJSON('密码不能为空!'))
     }
 
-    var sqlLogin = sql.login + "'" + username + "'";
+    // var sqlLogin = sql.login + "'" + username + "'";
     console.log('---------------- 查询单个进场信息 ----------------');
-    console.log(sqlLogin)
+    console.log(sql.login)
     console.log('---------------------------------------------------');
-    helper.query(sqlLogin, function(err, result) {
+    helper.queryArgs(sql.login, username, function(err, result) {
         if (err) {
             res.json(common.msyqlErrorAction(err));
         } else {
@@ -44,7 +46,7 @@ router.post('/login', function(req, res) {
                 res.json(common.resErrorJSON('用户名不存在!'))
             } else {
                 if (result[0].MM == password) {
-                	req.session.user = result[0];
+                    req.session.user = result[0];
                     res.json(common.resResultJSON(true, '登录成功！'));
                 } else {
                     res.json(common.resErrorJSON('密码不正确!'));
@@ -54,5 +56,22 @@ router.post('/login', function(req, res) {
     })
 })
 
+
+//角色权限
+router.get('/role', function(req, res) {
+    if (!req.session.user) {
+        res.redirect("/login");
+    }
+    var YH_ID = req.session.user.YH_ID;
+    console.log(req.session.user);
+    helper.queryArgs(sql.role, YH_ID, function(err, result) {
+        if (err) {
+        	console.log(err);
+            res.json(common.msyqlErrorAction(err));
+        } else {
+            res.json(common.resResultJSON(result, '查询用户权限成功！'));
+        }
+    })
+})
 
 module.exports = router;
